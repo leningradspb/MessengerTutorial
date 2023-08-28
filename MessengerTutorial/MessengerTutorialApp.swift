603217c5088597151572adcdba49036390322de5
Ed <Siniakov>
2023-08-28 21:01:32 +0300

    Initial Commit

---
+++ MessengerTutorial/MessengerTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerTutorialApp.swift
+//  MessengerTutorial
+//
+//  Created by Eduard Kanevskii on 28.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MessengerTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
