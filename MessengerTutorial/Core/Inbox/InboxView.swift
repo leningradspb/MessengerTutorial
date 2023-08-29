6b3b2903c29745c7d4e8281b5fb2c7b0e6828230
Ed <Siniakov>
2023-08-29 13:29:21 +0300

    toolbars added

--- MessengerTutorial/Core/Inbox/InboxView.swift
+++ MessengerTutorial/Core/Inbox/InboxView.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct InboxView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                
+            } // ScrollView
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    HStack {
+                        Image(systemName: "person.circle.fill")
+                        
+                        Text("Chats")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        Text("Show new message")
+                    } label: {
+                        Image(systemName: "square.and.pencil.circle.fill")
+                            .resizable()
+                            .frame(width: 32, height: 32)
+                            .foregroundStyle(.black, Color(.systemGray5))
+                    }
+
+                }
+            } // scrollview toolbar
+            
+            
+        } // NavigationStack
     }
 }
 
