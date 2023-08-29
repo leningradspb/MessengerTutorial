38d464221ade8916d4bc2585e8a6e3580b328328
Ed <Siniakov>
2023-08-29 12:56:31 +0300

    log in view

--- MessengerTutorial/Core/Auth/View/LoginView.swift
+++ MessengerTutorial/Core/Auth/View/LoginView.swift
@@ -8,8 +8,85 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Spacer()
+                // Image
+                Image("messenger-app-icon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .padding()
+                
+                // Text fields
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    
+                    SecureField("Enter your password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                }
+                
+                // Forgot password
+                Button {
+                    print("Forgot password")
+                } label: {
+                    Text("Forgot password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .padding(.trailing, 28)
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+
+                // Login button
+                Button {
+                    print("Log in ")
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+//                        .padding(.horizontal, 24)
+                        .frame(width: UIScreen.main.bounds.width - 24)
+                        .padding(.vertical)
+                        .background(Color(.systemBlue))
+                        .cornerRadius(10)
+                        
+                }
+                
+                Spacer()
+                
+                // Sign up
+                Divider() // - полоска серая
+                // не используется navigation destination потому что не передаем туда data. здесь просо отображение экрана по нажатию
+                NavigationLink {
+                    Text("Sign up view")
+                } label: {
+                    HStack {
+                        Text("Don't have an account?")
+                        
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                }
+                .padding(.vertical)
+                
+            }
+        }
     }
 }
 
