b6442f0d0cdff4302b8ba8435de41c1286088448
Ed <Siniakov>
2023-08-30 21:42:38 +0300

    char ui part 1

---
+++ MessengerTutorial/Core/Chat/ChatView.swift
@@ -0,0 +1,65 @@
+//
+//  ChatView.swift
+//  MessengerTutorial
+//
+//  Created by Eduard Kanevskii on 30.08.2023.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    @State private var messageText = ""
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                //header
+                VStack {
+                    CircularProfileImageView(user: User.mock_user, size: .xlarge)
+                    
+                    VStack(spacing: 4) {
+                        Text("Bruce Wayne")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                        
+                        Text("Messenger")
+                            .font(.footnote)
+                            .foregroundColor(.gray)
+                    }
+                }
+                
+                //messages
+                
+            }
+            
+            Spacer()
+            //input
+            // axis - чтобы растягивался
+            ZStack(alignment: .trailing) {
+                TextField("Messgage....", text: $messageText, axis: .vertical)
+                    .padding(12)
+                    .padding(.trailing, 49)
+                    .background(Color(.systemGroupedBackground))
+                    .clipShape(Capsule())
+                    .font(.subheadline)
+                
+                
+                Button {
+                    print("send tapped")
+                } label: {
+                    Text("Send")
+                        .fontWeight(.semibold)
+                }
+                .padding(.horizontal)
+                
+            }
+            .padding()
+        }
+    }
+}
+
+struct ChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatView()
+    }
+}
