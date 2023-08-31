e9510d54bed0a48e38966ef36bf1805535eef574
Ed <Siniakov>
2023-08-31 20:33:17 +0300

    log in

--- MessengerTutorial/Core/Auth/Service/AuthService.swift
+++ MessengerTutorial/Core/Auth/Service/AuthService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Firebase
+import FirebaseFirestoreSwift
 
 class AuthService {
     @Published var userSession: FirebaseAuth.User?
@@ -17,19 +18,30 @@ class AuthService {
         print("DEBUG: \(userSession?.uid)")
     }
     
-     func login(with email: String, password: String) async throws {
-        
+    @MainActor
+    func login(with email: String, password: String) async throws {
+        do {
+            let result = try await Auth.auth().signIn(withEmail: email, password: password)
+            self.userSession = result.user
+            print("Debug: log in user \(result.user.uid)")
+        } catch {
+            print("DEBUG: ERROR! login catch block \(error.localizedDescription)")
+        }
     }
     
+    @MainActor
     func createUser(with email: String, password: String, fullName: String) async throws {
-       do {
-           let result = try await Auth.auth().createUser(withEmail: email, password: password)
-           print("Debug: crated user \(result.user.uid)")
-       } catch {
-           print("DEBUG: ERROR! createUser catch block \(error.localizedDescription)")
-       }
+        do {
+            let result = try await Auth.auth().createUser(withEmail: email, password: password)
+            self.userSession = result.user
+            print("Debug: crated user \(result.user.uid)")
+            try await self.uploadUserData(email: email, fullName: fullName, id: result.user.uid)
+        } catch {
+            print("DEBUG: ERROR! createUser catch block \(error.localizedDescription)")
+        }
     }
     
+    @MainActor
     func signOut() {
         do {
             try Auth.auth().signOut()
@@ -37,5 +49,11 @@ class AuthService {
         } catch {
             print("Failed signOut")
         }
+    }
+    
+    private func uploadUserData(email: String, fullName: String, id: String) async throws {
+        let user = User(id: id, fullName: fullName, email: email)
+        guard let encodedUser = try? Firestore.Encoder().encode(user) else { return }
+        try await Firestore.firestore().collection("users").document(id).setData(encodedUser)
     }
 }
--- MessengerTutorial/Model/User.swift
+++ MessengerTutorial/Model/User.swift
@@ -6,14 +6,21 @@
 //
 
 import Foundation
+//import FirebaseFirestoreSwift
 
 struct User: Codable, Identifiable, Hashable {
-    var id = NSUUID().uuidString
+    let id: String
+//    var id = NSUUID().uuidString
+//    @DocumentID var uid: String?
     let fullName: String
     let email: String
     var profileImageURL: String?
+    
+//    var id: String {
+//        uid ?? NSUUID().uuidString
+//    }
 }
 
 extension User {
-    static let mock_user = User(fullName: "Bruce Wayne", email: "batman@mail.ru", profileImageURL: "messenger-app-icon")
+    static let mock_user = User(id: NSUUID().uuidString, fullName: "Bruce Wayne", email: "batman@mail.ru", profileImageURL: "messenger-app-icon")
 }
